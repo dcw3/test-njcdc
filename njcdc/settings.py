073283547a95cf6d2d5d0e465a93d26a15282283
dcw3 <dcw3@princeton.edu>
2018-04-15 14:34:04 -0400

    blah

--- njcdc/settings.py
+++ njcdc/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import dj_database_url
 import django_heroku
-import json
 from .settings_common import *
 
 ALLOWED_HOSTS = [".herokuapp.com/", "127.0.0.1", "njcdc.herokuapp.com"]
