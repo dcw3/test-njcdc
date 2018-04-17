82c6ab4175ed46bdad868e14bc2c87162a426863
dcw3 <dcw3@princeton.edu>
2018-04-16 20:34:53 -0400

    stuff

--- njcdc/settings_common.py
+++ njcdc/settings_common.py
@@ -56,14 +56,59 @@ MIDDLEWARE = [
 # simple cached data
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_bmemcached.memcached.BMemcached',
+#         'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
+#         'OPTIONS': {
+#                     'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
+#                     'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD')
+#             }
+#     }
+# }
+
+servers = os.environ['MEMCACHIER_SERVERS']
+username = os.environ['MEMCACHIER_USERNAME']
+password = os.environ['MEMCACHIER_PASSWORD']
+
 CACHES = {
     'default': {
-        'BACKEND': 'django_bmemcached.memcached.BMemcached',
-        'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
+        # Use pylibmc
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+
+        'LOCATION': servers,
+
         'OPTIONS': {
-                    'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
-                    'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD')
+            # Use binary memcache protocol (needed for authentication)
+            'binary': True,
+            'username': username,
+            'password': password,
+            'behaviors': {
+                # Enable faster IO
+                'no_block': True,
+                'tcp_nodelay': True,
+
+                # Keep connection alive
+                'tcp_keepalive': True,
+
+                # Timeout settings
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
+
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30,
             }
+        }
     }
 }
 
